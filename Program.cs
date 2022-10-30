db95d1fdc8d7d1e1468f8d168b77888a6c03ac2b
Konstantino123 <kt79@bk.ru>
2022-10-30 06:57:11 +0300

    new massive

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Например 5 элементов
+Console.WriteLine("Введите число например 5");
+int massive = Convert.ToInt32(Console.ReadLine());
+int[]array = new int[massive];
+for (int i = 0; i < massive; i++)
+{
+    array[i] = new Random().Next(massive+1);
+    Console.WriteLine(array[i] + ";");
+}
